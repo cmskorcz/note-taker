7c22d06a6a85612522d930f68f8e101a33b3bc88
Chase Skorczewski <chaseskorey@icloud.com>
2022-06-13 16:39:51 -0400

    added post route for db

--- routes/api-routes/notes.js
+++ routes/api-routes/notes.js
@@ -1,9 +1,32 @@
 const router = require('express').Router()
+const { resolveSoa } = require('dns');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const { notes } = require('../../db/notes.json')
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+})
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+
+    notes.push(newNote);
+
+    const newObj = {
+        'notes': notes
+    };
+
+    fs.writeFile(path.join(__dirname, '../../db/notes.json'), JSON.stringify(newObj), err => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.json(notes)
+
 })
 
 module.exports = router
